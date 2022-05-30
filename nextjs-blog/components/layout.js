8a4cf4f150cefb078e61bdbaf1443689a58ef418
Akin Fagbohun <a_fagbohun@yahoo.co.uk>
2022-05-30 11:54:52 +0100

    getting posts now

--- nextjs-blog/components/layout.js
+++ nextjs-blog/components/layout.js
@@ -14,7 +14,7 @@ export default function Layout({ children, home }) {
         <link rel="icon" href="/favicon.ico" />
         <meta
           name="description"
-          content="Learn how to build a personal website using Next.js"
+          content="Building my personal website blog using Next.js"
         />
         <meta
           property="og:image"
@@ -23,7 +23,7 @@ export default function Layout({ children, home }) {
           )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
         />
         <meta name="og:title" content={siteTitle} />
-        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="github:card" content="summary_large_image" />
       </Head>
       <header className={styles.header}>
         {home ? (
--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -1,19 +1,47 @@
 import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
+import { getSortedPostsData } from '../lib/posts';
 
-export default function Home() {
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
         <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+          Hello, I'm Akin. I'm a front-end developer exploring new technologies
+          and documenting my journey through software development.
         </p>
+        <p>
+          As part of my foray into Next.js, I added this blog to my portfolio
+          site, so it's only right that I use it to share my thoughts! Have a
+          read -I hope you enjoy!
+        </p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   );
