76ec0cf56d60295ed97778e91fa07af83a2b928d
Akin Fagbohun <a_fagbohun@yahoo.co.uk>
2022-06-09 20:38:34 +0100

    located module css files to styles.

--- nextjs-blog/components/icons/NorthcodersLogo.jsx
+++ nextjs-blog/components/icons/NorthcodersLogo.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import styles from '../footer.module.css';
+import styles from '../../styles/footer.module.css';
 
 export const NorthcodersLogo = () => {
   const [hover, setHover] = useState(false);
