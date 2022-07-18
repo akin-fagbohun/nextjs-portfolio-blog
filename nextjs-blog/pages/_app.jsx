525ec2d1247ae703b69fdcb0d8c9938b3c11a512
Akin Fagbohun <a_fagbohun@yahoo.co.uk>
2022-07-18 14:26:11 +0100

    wrapped apollo provider

--- nextjs-blog/pages/_app.jsx
+++ nextjs-blog/pages/_app.jsx
@@ -1,7 +1,14 @@
+import { ApolloProvider } from '@apollo/client';
+import client from '../lib/graphql';
+
 import '../styles/global.css';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  );
 }
 
 // Only uncomment this method if you have blocking data requirements for
