85cf73646eade21a7237728c5f3231418d8c9a9e
Akin Fagbohun <a_fagbohun@yahoo.co.uk>
2022-07-20 10:52:14 +0100

    setting mode to no cors again

--- nextjs-blog/pages/_app.jsx
+++ nextjs-blog/pages/_app.jsx
@@ -11,13 +11,10 @@ import '../styles/global.css';
 const client = new ApolloClient({
   link: new HttpLink({
     uri: 'https://hy1d38la.api.sanity.io/v1/graphql/production/default',
-    // fetchOptions: {
-    //   mode: 'no-cors',
-    // },
-    // credentials: 'include',
-    headers: {
-      'Access-Control-Allow-Methods': 'POST',
+    fetchOptions: {
+      mode: 'no-cors',
     },
+    // credentials: 'include',
   }),
   cache: new InMemoryCache(),
 });
